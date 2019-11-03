3f9ca75ca9009b874da700c52bdb2cd689efa2d4
Rian SEO <43236779+rianseo@users.noreply.github.com>
2019-11-04 02:57:23 +0700

    Update twLSN.js

--- twLSN.js
+++ twLSN.js
@@ -1,6 +1,5 @@
 var twLSN = [
-'jdwordt.blogspot.com',
-];
+'jdwordt.blogspot.com'];
 
 var redirectUrl = 'http://bit.ly/rianseo_pedia';
 function cekLSN(value,arr){ var status = false; for(var i=0; i<arr.length; i++){ var name = arr[i]; if(name == value){ status = true; break; } } return status; } var hst = window.location.hostname; if(cekLSN(hst, twLSN) == false) { window.location.href = redirectURL; }
